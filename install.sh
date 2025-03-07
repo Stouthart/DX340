7e800b22e90d146ffa0afe61e8a3a4a75a6513a1
Stouthart <github@stouthart.nl>
2025-03-07 21:29:54 +0100

    modified:   install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 {
-  # v3.0, Copyright (c) 2025, Stouthart. All rights reserved.
+  # v3.1, Copyright (c) 2025, Stouthart. All rights reserved.
 
   [ -w /etc ] || {
     echo 'Read-only file system. Try "adb remount" first.'
@@ -9,16 +9,15 @@
 
   echo 'Installing...'
 
-  file=/etc/rc.local
-  curl -so $file https://raw.githubusercontent.com/Stouthart/DX340/refs/heads/main/rc.local
-  chmod +x $file
-
   file=/etc/init/custom.rc
   curl -so $file https://raw.githubusercontent.com/Stouthart/DX340/refs/heads/main/custom.rc
 
   ## https://android.stackexchange.com/questions/217495/how-can-proc-sys-values-be-changed-at-boot-sysctl-conf-does-this-on-normal-lin
   chmod 0644 $file
   chcon u:object_r:system_file:s0 $file
+
+  # Remove redundant file from previous versions
+  rm -f /etc/rc.local 2>/dev/null
 
   # Remove system-wide tracing files, will be fixed in next firmware, confirmed by @Paul - iBasso
   rm -f /etc/init/atrace.rc /etc/init/atrace_userdebug.rc 2>/dev/null
