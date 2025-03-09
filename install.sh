0b860b356cc5adf262077df1a39c22c30cc14fa3
Stouthart <github@stouthart.nl>
2025-03-09 08:29:28 +0100

    modified:   install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# v4.1, Copyright (c) 2025 Stouthart. All rights reserved.
+# v4.1, Copyright (C) 2025 Stouthart. All rights reserved.
 {
   [ -w /etc ] || {
     echo 'Read-only file system. Try "adb remount" first.'
