867f5a8bfbfd1c56c2ab1611c8f22e1686e019b7
Stouthart <github@stouthart.nl>
2025-03-19 17:37:42 +0100

    modified:   install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # shellcheck disable=SC2154
 #
-# v5.1, Copyright (C) 2025 Stouthart. All rights reserved.
+# v5.2b, Copyright (C) 2025 Stouthart. All rights reserved.
 {
   # shellcheck disable=SC3028
   [ "$HOSTNAME" = DX340 ] || {
@@ -23,14 +23,16 @@
   chcon u:object_r:system_file:s0 $file
 
   [ "$pmax" = 1 ] && { # "Performance MAX"
-    LC_ALL=C sed -i 's,### noidle$,exec_background -- /system/bin/dumpsys deviceidle disable,' $file
     minfreq=1401600
+    sed -i 's,### noidle$,exec_background -- /system/bin/dumpsys deviceidle disable,' $file
   }
 
+  [ "$psave" = 1 ] && minfreq=902400 # "Power SAVE"
+
   case "$minfreq" in
-  652800 | 902400 | 1401600)
+  902400 | 1401600)
     cmd="sleep 1; echo -n ${minfreq} >/sys/devices/system/cpu/cpufreq/policy4/scaling_min_freq"
-    LC_ALL=C sed -i "s,### minfreq$,exec_background -- ${SHELL} -c \"${cmd}\"," $file
+    sed -i "s,### minfreq$,exec_background -- ${SHELL} -c \"${cmd}\"," $file
     ;;
   esac
 
@@ -39,9 +41,7 @@
     sed -i -E 's,(top-app/schedtune.boost) [0-9]+$,\1 40,' $file
   }
 
-  [ -x /etc/rc.local ] && {
-    LC_ALL=C sed -i 's,### debug$,exec_background -- /etc/rc.local,' $file
-  }
+  [ -x /etc/rc.local ] && sed -i 's,### debug$,exec_background -- /etc/rc.local,' $file
 
   sed -i -E "s,### printk ([a-z]+)$,write /dev/kmsg \"${file##*/}: \1\",g" $file
   sed -i -E 's,### [a-z]+$,# N/A,g' $file
